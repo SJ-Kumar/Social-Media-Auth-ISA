f8da107613e7ba3fb96538e79e1866b5a1991626
S Jayanth Kumar <90379900+SJ-Kumar@users.noreply.github.com>
2023-07-06 11:47:25 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -69,7 +69,7 @@ const App = () => {
   }
 
   return (
-    <section className="bg-emerald-500 flex items-center justify-center h-screen">
+    <section className="bg-twitter-blue flex items-center justify-center h-screen">
       <div>
         <Toaster toastOptions={{ duration: 4000 }} />
         <div id="recaptcha-container"></div>
@@ -80,7 +80,7 @@ const App = () => {
         ) : (
           <div className="w-80 flex flex-col gap-4 rounded-lg p-4">
             <h1 className="text-center leading-normal text-white font-medium text-3xl mb-6">
-              Welcome to <br /> CODE A PROGRAM
+              Welcome to <br /> Twitter
             </h1>
             {showOTP ? (
               <>
